2297eea17f0afb7285f8261bf6c5654ac7fd8f7b
Fuszenecker Róbert <robert.fuszenecker@outlook.com>
2023-08-25 19:43:37 +0200

    Analyzer.

--- src/order/Program.cs
+++ src/order/Program.cs
@@ -1,2 +1,68 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Xml.Linq;
+
+var dictionary = XElement.Load("Dictionaries/lat.ls.perseus-eng2.xml");
+
+var taglist = new List<List<string>>();
+var leveltags = new List<string>();
+var finished = false;
+
+do
+{
+    leveltags = new List<string>();
+    taglist.Add(leveltags);
+    finished = Analyze(dictionary);
+
+    foreach (var tags in taglist.SkipLast(1))
+    {
+        var remove = new List<string>();
+
+        foreach (var tag in tags)
+        {
+            if (leveltags.Contains(tag))
+            {
+                remove.Add(tag);
+            }
+        }
+
+        foreach (var tag in remove)
+        {
+            tags.Remove(tag);
+        }
+    }
+} while (!finished);
+
+foreach (var tags in taglist)
+{
+    if (tags.Count > 0)
+    {
+        Console.WriteLine(String.Join(", ", tags));
+    }
+}
+
+bool Analyze(XElement node)
+{
+    var descendants = node.Elements();
+
+    if (descendants.Count() > 0)
+    {
+        foreach (var child in descendants)
+        {
+            Analyze(child);
+        }
+
+        return false;
+    }
+    else
+    {
+        if (!leveltags.Contains(node.Name.ToString()))
+        {
+            leveltags.Add(node.Name.ToString());
+        }
+
+        if (node.Parent != null)
+            node.Remove();
+
+        return true;
+    }
+}
